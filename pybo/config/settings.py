ac26d5f61f4337cc971c3c16363494997d41fdab
parlyresk <72953981+parlyresk@users.noreply.github.com>
2022-12-22 14:52:20 +0900

    Update settings.py

--- pybo/config/settings.py
+++ pybo/config/settings.py
@@ -82,10 +82,14 @@ CRONJOBS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER' : 'vagrant',
+        'PASSWORD' : 'vagrant',
+        'HOST' : 'mycluster.mysql-cluster.svc.cluster.local',
+        'PORT' : '3306',
+    },
 }
 
 
